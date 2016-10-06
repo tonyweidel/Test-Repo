28862bbf6f0c6ee77c8d3cdfa90ae87bf0fbcbda
Tony Weidel <8aweidel@stpatrick.org>
2016-10-06 14:10:25 -0500

    Starting Octocat constructor

---
+++ octocats.js
@@ -0,0 +1,20 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function octoCat(n,a) {
+  this.name = n;
+  this.arms = s;
+  this.slap = function() {
+    for(var a=0);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
